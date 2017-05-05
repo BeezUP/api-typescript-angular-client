499d31bf9d8e173d70dee18e0b3c62adb938678b
Automated VSTS BeezUP Integration <johnny@beezup.com>
2017-05-05 13:12:49 +0200

    automatic commit from VSO

--- model/OrderLinks.ts
+++ model/OrderLinks.ts
@@ -14,5 +14,6 @@ import * as models from './models';
 
 export interface OrderLinks {
     "self": models.DefinitionslinksGetOrderLink;
+    "history"?: models.DefinitionslinksGetOrderHistoryLink;
 }
 
